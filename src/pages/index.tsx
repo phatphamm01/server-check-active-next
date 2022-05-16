7606d5c9a6dcddaae4ac07cdf97fd5be50e0ef57
PhatPhamFe <phatphamm@usumsoftware.com>
2022-05-17 00:47:37 +0700

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,27 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { Socket } from 'socket.io';
 import SocketIOClient from 'socket.io-client';
 
 const Home: NextPage = () => {
   const router = useRouter();
   const { user } = router.query;
   const [connected, setConnected] = useState(false);
+  let refSocket = useRef<any>();
+
+  useEffect(() => {
+    refSocket.current = SocketIOClient({
+      path: '/api/socketio',
+    });
+    refSocket.current.on('connect', () => {
+      console.log('SOCKET CONNECTED!', refSocket.current.id);
+      setConnected(true);
+    });
+  }, []);
 
   useEffect((): any => {
-    if (user) {
-      const socket = SocketIOClient({
-        path: '/api/socketio',
-      });
-
-      socket.on('connect', () => {
-        console.log('SOCKET CONNECTED!', socket.id);
-        socket.emit('active', user);
-        setConnected(true);
-      });
-
-      if (socket) return () => socket.disconnect();
-    }
+    refSocket.current.emit('active', user);
   }, [user]);
 
   return <div>Con gà con: {connected ? 'Đã kết nối' : 'Chưa kết nối'}</div>;
